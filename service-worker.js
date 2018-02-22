dd91644bfb6681cd131c45abe541904bb150d684
Leonardo Mendonça <leonarede@gmail.com>
2018-02-21 22:38:06 -0300

    inicio

--- service-worker.js
+++ service-worker.js
@@ -29,3 +29,64 @@ self.toolbox.router.any('/*', self.toolbox.fastest);
 // for any other requests go to the network, cache,
 // and then only use that cached resource if your user goes offline
 self.toolbox.router.default = self.toolbox.networkFirst;
+
+
+var files = [
+  './build/main.js',
+  './build/vendor.js',
+  './build/main.css',
+  './build/polyfills.js',
+  'index.html',
+  'manifest.json'
+];
+// dev only
+if (typeof files == 'undefined') {
+  var files = [];
+} else {
+  files.push('./');
+}
+
+var CACHE_NAME = 'soudobairro-v1';
+
+self.addEventListener('activate', function(event) {
+  console.log('[SW] Activate');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME.indexOf(cacheName) == -1) {
+            console.log('[SW] Delete cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('install', function(event){
+  console.log('[SW] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return Promise.all(
+        files.map(function(file){
+          return cache.add(file);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('[SW] fetch ' + event.request.url)
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request.clone());
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event);
+  clients.openWindow('/');
+});
