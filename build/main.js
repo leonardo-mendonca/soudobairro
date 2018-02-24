03d42aa87fe1e6cd8645acec1d97ea76d78d4952
Leonardo Mendonça <leonarede@gmail.com>
2018-02-24 18:18:49 -0300

    inicio

--- build/main.js
+++ build/main.js
@@ -80,6 +80,41 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 
 
 
@@ -95,26 +130,51 @@ var LoginPage = (function () {
         this.user = {};
     }
     LoginPage.prototype.login = function (user) {
-        var _this = this;
-        var loading = this.loadingCtrl.create({
-            spinner: 'bubbles',
-            content: 'Aguarde... Efetuando Login',
-            duration: 3000
-        });
-        loading.present();
-        this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password).then(function () {
-            loading.dismiss();
-            _this.navCtrl.setRoot(__WEBPACK_IMPORTED_MODULE_4__home_home__["a" /* HomePage */]);
-            _this.toast.create({
-                message: 'Bem-vindo ao SouDoBairro',
-                duration: 3000
-            }).present();
-        }).catch(function (error) {
-            console.error(error.code);
-            _this.toast.create({
-                message: error,
-                duration: 3000
-            }).present();
+        return __awaiter(this, void 0, void 0, function () {
+            var _this = this;
+            var loading;
+            return __generator(this, function (_a) {
+                loading = this.loadingCtrl.create({
+                    spinner: 'bubbles',
+                    content: 'Aguarde... Efetuando Login',
+                    duration: 3000
+                });
+                loading.present();
+                this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password).then(function () {
+                    loading.dismiss();
+                    _this.navCtrl.setRoot(__WEBPACK_IMPORTED_MODULE_4__home_home__["a" /* HomePage */]);
+                    _this.toast.create({
+                        message: 'Bem-vindo ao SouDoBairro',
+                        duration: 3000
+                    }).present();
+                }).catch(function (error) {
+                    loading.dismiss();
+                    var erro;
+                    switch (error.code) {
+                        case "auth/user-not-found": {
+                            erro = "Usuário e/ou Senha incorretos";
+                            break;
+                        }
+                        case "auth/invalid-email": {
+                            erro = "Usuário e/ou Senha incorretos";
+                            break;
+                        }
+                        case "auth/invalid-password": {
+                            erro = "Usuário e/ou Senha incorretos";
+                            break;
+                        }
+                        default: {
+                            erro = "Usuário e/ou Senha incorretos";
+                            break;
+                        }
+                    }
+                    _this.toast.create({
+                        message: erro,
+                        duration: 3000
+                    }).present();
+                });
+                return [2 /*return*/];
+            });
         });
     };
     LoginPage.prototype.register = function () {
@@ -130,7 +190,7 @@ var LoginPage = (function () {
     };
     LoginPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-login',template:/*ion-inline-start:"C:\IONIC\sou-do-bairro\src\pages\login\login.html"*/'<ion-header>\n\n  <ion-navbar color="primary">\n    <ion-title>Sou de Santa Bárbara</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n<ion-content no-bounce>\n  <ion-slides>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-1.png" class="slide-image"/>\n      <div id="bairro">SANTA BÁRBARA</div>\n      <div id="slogan">Valorize o comércio do seu Bairro!!!</div>\n      <h2 class="slide-title">slide.title</h2>\n      <p>slide.description</p>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-2.png" class="slide-image"/>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-3.png" class="slide-image"/>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-4.png" class="slide-image"/>\n\n          <ion-list>\n              <ion-item>\n                <ion-label color="primary">Email</ion-label>\n                <ion-input type="email" placeholder="fulano@provedor.com" [(ngModel)]="user.email"></ion-input>\n              </ion-item> \n              <ion-item>\n                <ion-label color="primary">Senha</ion-label>\n                <ion-input type="password" placeholder="sua senha" [(ngModel)]="user.password"></ion-input>\n              </ion-item>  \n              <ion-item>\n                  <button ion-button (click)="login(user)" full large>Entrar</button>\n                  <button ion-button clear (click)="register()" full large>Cadastrar</button>      \n              </ion-item>                    \n            </ion-list>      \n    </ion-slide>\n  </ion-slides>\n</ion-content>\n'/*ion-inline-end:"C:\IONIC\sou-do-bairro\src\pages\login\login.html"*/,
+            selector: 'page-login',template:/*ion-inline-start:"C:\IONIC\sou-do-bairro\src\pages\login\login.html"*/'<ion-header>\n\n  <ion-navbar color="primary">\n    <ion-title>SouDoBairro de Santa Bárbara</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n<ion-content no-bounce>\n  <ion-slides pager="true">\n    <ion-slide>\n      <img src="./assets/imgs/logo_bairro.png" class="slide-image"/>\n      <div id="bairro">SANTA BÁRBARA</div>\n      <div id="slogan">Valorize o comércio do seu Bairro!!!</div>\n      <h2 class="slide-title">Agora o seu Bairro tem um App</h2>\n      <p class="slide-descricao">Chegou o SouDoBairro, um app para unir consumidor e comerciante que moram no mesmo bairro e que por algum motivo ainda não se conhecem.</p>\n      <p class="slide-descricao">Deslize para continuar...</p>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-2.png" class="slide-image"/>\n      <h2 class="slide-title">O que temos no App?</h2>\n      <p class="slide-descricao">Aqui você poderá se conectar com os chamados "papa-táxis" do bairro e contratar uma corrida. Você também pode encontrar um produto de forma rápida, se conectando com vendedores do seu bairro.</p>\n      <p class="slide-descricao">Deslize para continuar...</p>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-3.png" class="slide-image"/>\n      <h2 class="slide-title">Saia na frente...</h2>\n      <p class="slide-descricao">Você pode fazer uma pesquisa e saber quais produtos estão sendo vendidos no seu bairro e quais produtos não possuem vendedores perto de você, criando assim uma excelente estratégia de negócio.</p>\n      <p class="slide-descricao">Deslize para continuar...</p>\n    </ion-slide>\n    <ion-slide>\n      <img src="./assets/imgs/ica-slidebox-img-4.png" class="slide-image ultima"/>\n      <h2 class="slide-title">Vamos começar?</h2>\n      <div class="comecar">É incrivelmente grátis... Se já é cadastrado, comece informando seus dados e clique em Entrar, se não, clique em Cadastrar...</div>\n      <ion-list>\n        <ion-item>\n          <ion-label color="primary">Email</ion-label>\n          <ion-input type="email" placeholder="fulano@provedor.com" [(ngModel)]="user.email"></ion-input>\n        </ion-item> \n        <ion-item>\n          <ion-label color="primary">Senha</ion-label>\n          <ion-input type="password" placeholder="sua senha" [(ngModel)]="user.password"></ion-input>\n        </ion-item>  \n        <ion-item>\n            <button ion-button (click)="login(user)" full large>Entrar</button>\n            <button ion-button clear (click)="register()" full large>Cadastrar</button>      \n        </ion-item>                    \n      </ion-list>      \n    </ion-slide>\n  </ion-slides>\n</ion-content>\n'/*ion-inline-end:"C:\IONIC\sou-do-bairro\src\pages\login\login.html"*/,
         }),
         __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2_angularfire2_auth__["a" /* AngularFireAuth */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2_angularfire2_auth__["a" /* AngularFireAuth */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* ToastController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* ToastController */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* LoadingController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* LoadingController */]) === "function" && _e || Object])
     ], LoginPage);
@@ -198,40 +258,58 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 
 
 var RegisterPage = (function () {
-    function RegisterPage(afAuth, navCtrl, navParams) {
+    function RegisterPage(afAuth, navCtrl, navParams, toast, loadingCtrl) {
         this.afAuth = afAuth;
         this.navCtrl = navCtrl;
         this.navParams = navParams;
+        this.toast = toast;
+        this.loadingCtrl = loadingCtrl;
         this.user = {};
     }
     RegisterPage.prototype.register = function (user) {
         return __awaiter(this, void 0, void 0, function () {
-            var result, e_1;
+            var loading, result, e_1;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
-                        _a.trys.push([0, 2, , 3]);
-                        return [4 /*yield*/, this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password)];
+                        loading = this.loadingCtrl.create({
+                            spinner: 'bubbles',
+                            content: 'Aguarde... Efetuando Login',
+                            duration: 3000
+                        });
+                        loading.present();
+                        _a.label = 1;
                     case 1:
-                        result = _a.sent();
-                        console.log(result);
-                        return [3 /*break*/, 3];
+                        _a.trys.push([1, 3, , 4]);
+                        return [4 /*yield*/, this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password)];
                     case 2:
+                        result = _a.sent();
+                        this.toast.create({
+                            message: result,
+                            duration: 3000
+                        }).present();
+                        return [3 /*break*/, 4];
+                    case 3:
                         e_1 = _a.sent();
-                        console.error(e_1);
-                        return [3 /*break*/, 3];
-                    case 3: return [2 /*return*/];
+                        loading.dismiss();
+                        this.toast.create({
+                            message: e_1,
+                            duration: 3000
+                        }).present();
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
                 }
             });
         });
     };
     RegisterPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-register',template:/*ion-inline-start:"C:\IONIC\sou-do-bairro\src\pages\register\register.html"*/'<ion-header>\n\n  <ion-navbar>\n    <ion-title>Register</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n    <ion-item>\n        <ion-label floating>Email</ion-label>\n        <ion-input type="text" [(ngModel)]="user.email"></ion-input>\n      </ion-item>\n    \n      <ion-item>\n        <ion-label floating>Senha</ion-label>\n        <ion-input type="password" [(ngModel)]="user.password"></ion-input>\n      </ion-item>\n    \n      <button ion-button (click)="register(user)">Register</button>\n      \n\n</ion-content>\n'/*ion-inline-end:"C:\IONIC\sou-do-bairro\src\pages\register\register.html"*/,
+            selector: 'page-register',template:/*ion-inline-start:"C:\IONIC\sou-do-bairro\src\pages\register\register.html"*/'<ion-header>\n\n  <ion-navbar>\n    <ion-title>Cadastro</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content no-bounce>\n\n  <ion-list>\n    <ion-item class="item1">\n      <img src="./assets/imgs/logo_bairro.png" class="slide-image"/>\n      <div id="bairro">SANTA BÁRBARA</div>\n      <h2 class="slide-title">Cadastro Rápido</h2>\n    </ion-item>\n    <ion-item>\n      <ion-label color="primary">Email</ion-label>\n      <ion-input type="email" placeholder="Digite seu melhor email" [(ngModel)]="user.email"></ion-input>\n    </ion-item> \n    <ion-item>\n      <ion-label color="primary">Senha</ion-label>\n      <ion-input type="password" placeholder="Informe uma Senha" [(ngModel)]="user.password"></ion-input>\n    </ion-item>  \n    <ion-item>\n        <button ion-button (click)="register(user)" full large>Cadastrar</button>              \n    </ion-item>                    \n  </ion-list> \n      \n\n</ion-content>\n'/*ion-inline-end:"C:\IONIC\sou-do-bairro\src\pages\register\register.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2_angularfire2_auth__["a" /* AngularFireAuth */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]])
+        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2_angularfire2_auth__["a" /* AngularFireAuth */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2_angularfire2_auth__["a" /* AngularFireAuth */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]) === "function" && _c || Object, typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* ToastController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* ToastController */]) === "function" && _d || Object, typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* LoadingController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* LoadingController */]) === "function" && _e || Object])
     ], RegisterPage);
     return RegisterPage;
+    var _a, _b, _c, _d, _e;
 }());
 
 //# sourceMappingURL=register.js.map
